a0146e5255ac04f3430fa527040f5576f90ed693
Merc4tor <joostkkoch@gmail.com>
2025-02-24 00:08:33 +0100

    new key

--- vuejs/src/firebase.js
+++ vuejs/src/firebase.js
@@ -4,16 +4,8 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { ref } from 'vue'; // NO LONGER NEEDED HERE. Pinia handles reactivity
+import firebaseConfig from './firebaseConfig.json'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAz5WwzYx5dkI8VlBH0qoXTduNisCdmfaw",
-  authDomain: "toetspws.firebaseapp.com",
-  projectId: "toetspws",
-  storageBucket: "toetspws.firebasestorage.app",
-  messagingSenderId: "771520566941",
-  appId: "1:771520566941:web:76dd9b37e4cb84e6f6a674",
-  measurementId: "G-4W9YL1FRW7"
-};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
